84278efee916595d187dc0b546ca155019b44d18
jmjar <jmjaramillo95@gmail.com>
2026-08-09 22:02:04 -0500

    vehiculo-constructor

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -5,4 +5,19 @@ public class Vehiculo {
     String color;
     double tarifa;
     boolean disponible;
+
+    public Vehiculo(String matricula,
+                    String marca,
+                    String modelo,
+                    String color,
+                    double tarifa)
+    {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo= modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        this.disponible = false;
+    }
+
 }
