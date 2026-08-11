4b843c1fa00a0e5ecb1e06df1dba218f1719308d
jmjar <jmjaramillo95@gmail.com>
2026-08-10 22:13:32 -0500

    vehiculo2

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    Vehiculo vehiculo = new Vehiculo("4050 ABJ", "VW", "GTI", "Blanco", 100.0);
+    Vehiculo vehiculo1 = new Vehiculo("4050 ABJ", "VW", "GTI", "Blanco", 100.0);
+    Vehiculo vehiculo2 = new Vehiculo("2345 JVM", "Seat", "León", "Negro", 80.0);
 
-    System.out.println(vehiculo.color);
+    System.out.println(vehiculo1.color);
 
 }
