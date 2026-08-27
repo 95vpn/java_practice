e66f13e14ecc14bcee784091ad20749988b75085
jmjar <jmjaramillo95@gmail.com>
2026-08-26 23:35:58 -0500

    mis-vehiculos

--- src/MisVehiculos.java
+++ src/MisVehiculos.java
@@ -3,4 +3,11 @@ public class MisVehiculos {
     {
         v.setDisponible(true);
     }
+    public static void main (String args[])
+    {
+        Vehiculo vehiculo1 = new Vehiculo("4060 ABJ",
+                "VW", "GTI", "Blanco", 100.0);
+
+        Vehiculo vehiculo2 = new Vehiculo("2345 JVM", "SEAT", "León", "Negro", 80.0);
+    }
 }
