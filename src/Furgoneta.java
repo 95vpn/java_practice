518d2eeff7870c3e1671739beba3f2f81fd84219
jmjar <jmjaramillo95@gmail.com>
2026-08-30 17:48:16 -0500

    fujrgoneta

---
+++ src/Furgoneta.java
@@ -0,0 +1,35 @@
+public class Furgoneta extends Vehiculo
+{
+    private int carga;
+    private int volumen;
+
+    public Furgoneta(String matricula,
+                     String marca,
+                     String modelo,
+                     String color,
+                     double tarifa,
+                     int carga,
+                     int volumen)
+    {
+        super(matricula, marca, modelo, color, tarifa);
+        this.carga = carga;
+        this.volumen = volumen;
+    }
+
+    public int getCarga()
+    {
+        return this.carga;
+    }
+
+    public int getVolumen()
+    {
+        return this.volumen;
+    }
+
+    public String getAtributos()
+    {
+        return super.getAtributos() +
+                " Cargs (Kg): " + this.carga +
+                " Volumen (m3): " + this.volumen;
+    }
+}
