bb8e98c8a69397a4eb5b05a6698e9154998ca35f
jmjar <jmjaramillo95@gmail.com>
2026-09-09 05:50:41 -0500

    paquetes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Calendar;
+
 void main() {
+
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     //Vehiculo vehiculo1;
+    int edad, diaHoy, mesHoy, añoHoy;
+    diaHoy= Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+    System.out.println(diaHoy);
     Vehiculo vehiculo3;
     Vehiculo vehiculo1 = new Vehiculo("4050 ABJ", "VW", "GTI", "Blanco", 100.0);
     Vehiculo vehiculo2 = new Vehiculo("2345 JVM", "Seat", "León", "Negro");
